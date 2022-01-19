d82c8a169b65e0c1a4bef4a8e87c23612cffc010
Andrey Sidorov <andrey.sidorov@iag.com.au>
2022-01-19 15:43:36 +1100

    more resolution code

--- pages/api/version.js
+++ pages/api/version.js
@@ -1,8 +1,27 @@
-import semverMaxSatisfying from 'semver/ranges/max-satisfying'
-//const versions = ['1.1.0', '1.4.0', '1.2.0', '0.0.1', '3.0.1', '2.2.1', '2.5.3', '1.1.1'];
-import versions from '../../registry.json';
+import semverMaxSatisfying from "semver/ranges/max-satisfying";
+import versions from "../../registry.json";
 
 export default function handler(req, res) {
   const { name, range } = req.query;
-  res.status(200).json({ name: 'John Doe', name, range, versions })
+
+  if (!name) {
+    res.status(400).send("Missing name");
+  }
+  if (!range) {
+    res.status(400).send("Missing range");
+  }
+  const pkgInfo = versions[name];
+  if (!pkgInfo) {
+    res.status(404).send("Package not found");
+  }
+  const packageVersions = Object.keys(pkgInfo.versions);
+  const version = semverMaxSatisfying(packageVersions, range);
+  res
+    .status(200)
+    .json({
+      name: "John Doe",
+      name,
+      range,
+      entry: pkgInfo.versions[version].entry,
+    });
 }
