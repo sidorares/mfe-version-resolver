c61ca620c570461c1ad97b2e2e1a6094bf958a26
Andrey Sidorov <andrey.sidorov@iag.com.au>
2022-01-19 15:37:59 +1100

    initial resolution code

--- pages/api/version.js
+++ pages/api/version.js
@@ -1,3 +1,8 @@
+import semverMaxSatisfying from 'semver/ranges/max-satisfying'
+//const versions = ['1.1.0', '1.4.0', '1.2.0', '0.0.1', '3.0.1', '2.2.1', '2.5.3', '1.1.1'];
+import versions from '../../registry.json';
+
 export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe', query: req.query })
-  }
+  const { name, range } = req.query;
+  res.status(200).json({ name: 'John Doe', name, range, versions })
+}
